4872ade0fe20f49bd4ce4b383148db0113fd7b9b
wafachaari <wafachaari30@gmail.com>
2021-04-26 13:00:56 +0200

    updated

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ const passport = require('passport');
 require('./passport');
 const cors = require('cors');
 let allowedOrigins = [
-  '*',
-  /*  'http://localhost:8080/',
+  'http://localhost:8080/',
   'https://movie-api-db-30.herokuapp.com/',
-  'http://localhost:1234',*/
+  'http://localhost:1234',
 ];
 
 const app = express();
